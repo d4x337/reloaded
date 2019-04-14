58a48ae045683595d79d2dec259cdc3c937f99a6
d4x337 <d4x337@protonmail.com>
2019-04-14 07:34:06 +0200

    Picture model mass assigment vulnerability fix

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,8 +2,8 @@ class Picture < ActiveRecord::Base
 
   belongs_to :user, :foreign_key => :user_id
   validates :user_id, :pic,:presence => true
+  attr_accessible :user_id, :album_id, :caption, :pic_file_name, :pic_content_type,:pic_file_size
 
-  #has_attached_file :pic, :url => "/:class/:attachment/:id/:style_:basename.:extension"
   has_attached_file :pic, :styles => { :medium => "300x300>",:thumb => "160x160>" }
 
   validates_attachment 	:pic,
