53e77291af97c01f10b93e2e961f614fa162e209
d4x337 <d4x337@protonmail.com>
2019-04-14 07:55:40 +0200

    MiniPostTag model mass assigment vulnerability fix

--- app/models/mini_post_tag.rb
+++ app/models/mini_post_tag.rb
@@ -2,4 +2,5 @@ class MiniPostTag < ActiveRecord::Base
   has_many :tag, :foreign_key => :id
   has_many :mini_post,:foreign_key => :mini_post_id
   validates :mini_post_id, :tag_id, :presence => true
+  attr_accessible :mini_post_id, :tag_id
 end
