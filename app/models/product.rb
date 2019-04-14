2bdfdd957eedc2c0f84ca62ba01e69eb864c940c
d4x337 <d4x337@protonmail.com>
2019-04-14 06:40:22 +0200

    Product model mass assigment vulnerability fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,10 @@ class Product < ActiveRecord::Base
 
 # belongs_to :category
   has_many :cart_products
-  
+
+  attr_accessible :name, :description, :summary, :final_price, :costs, :category, :currency, :quota, :subscription, :pay_once, :in_stock, :promo, :active, :photo_file_name, :photo_content_type, :photo_file_size, :photo_updated_at
+
+
   PRODUCTYPES = %w[service product]
   PAYMENTS = %w[single free unatantum annual]
   validates :name,:category,:final_price,:presence => true
