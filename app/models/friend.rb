4a3e443b100951af54a6a2c2f2835d21b2e578fc
d4x337 <d4x337@protonmail.com>
2019-04-14 07:53:57 +0200

    Friend model mass assigment vulnerability fix

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,17 +1,12 @@
 class Friend < ActiveRecord::Base
   
   FRIEND_TYPES = %w[FRIEND CLOSE_FRIEND CUSTOM ACQUAINTANCE COLLEGUE PARENT, ENEMY]
-  
   belongs_to :user
-  
   validates :user_id,:friend_id, :presence => true
-  
   validates :relation, :inclusion => { :in => FRIEND_TYPES }
-
   after_create :send_added_to_ring_message
-  
- # after_destroy :send_removed_from_ring_message
-  
+  attr_accessible :user_id,:friend_id,:deleted
+
   private
   def send_added_to_ring_message
     UserMailer.added_to_ring(self.id).deliver
