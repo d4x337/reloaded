764dc53565e180e2dff3e6b935b514381c65af3c
d4x337 <d4x337@protonmail.com>
2019-04-14 07:52:08 +0200

    Cart Product  model mass assigment vulnerability fix

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,6 +1,6 @@
 class CartProduct < ActiveRecord::Base
   belongs_to :cart,:foreign_key => :cart_id
   belongs_to :product, :foreign_key => :product_id
-  
+  attr_accessible :cart_id,:product_id,:quantity,:single_price, :total_price, :nickname, :domain,:status
   validates :cart_id, :product_id, :presence => true
 end
