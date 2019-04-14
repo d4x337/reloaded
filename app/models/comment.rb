aadccd7c52bea1abec4ab9d7a15a3456dc708757
d4x337 <d4x337@protonmail.com>
2019-04-14 07:13:56 +0200

    Comment  model mass assigment vulnerability fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
   
   belongs_to :post
+  attr_accessible :post_id,:author,:author_email,:author_url,:author_ip,:date,:date_gmt,:content,:karma,:approved,:agent,:type,:parent,:user_id
+
   validates :post_id    ,:presence => true
   validates :author     ,:presence => true, :length => { :minimum => 3 }
   validates :content    ,:presence => true, :length => { :minimum => 3 }
