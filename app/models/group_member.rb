1a12b2fe88301fdf7e5d607fd48b74e5b80664e8
d4x337 <d4x337@protonmail.com>
2019-04-14 07:05:54 +0200

    GroupMember  model mass assigment vulnerability fix

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -1,5 +1,5 @@
 class GroupMember < ActiveRecord::Base
-  # attr_accessible :title, :body
+   attr_accessible :group_id, :user_id, :role, :invited_by, :promoted_by, :removed_by, :join_date, :leave_date, :active
    belongs_to :user
    belongs_to :group
   
