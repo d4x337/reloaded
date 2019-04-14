2128fc062de3f1d452caf1a9c102dc704eaca934
d4x337 <d4x337@protonmail.com>
2019-04-14 07:49:22 +0200

    Ads model mass assigment vulnerability fix

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,14 +1,7 @@
 class Ad < ActiveRecord::Base
  
   validates :title,:content,:presence => true
- 
   has_attached_file :image, :url => "/:class/:attachment/:id/:basename.:extension"
- 
   validates_attachment_content_type :image, :content_type =>['image/jpeg', 'image/png', 'image/gif']
- 
- 
- # include PublicActivity::Model
- # tracked
-  
- 
+  attr_accessible :title, :content, :url, :action, :active, :expire_at, :customer_id, :image_file_name, :image_content_type,:image_file_size,:type, :locale, :viewed, :visibility
 end
