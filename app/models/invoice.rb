fe853c0b690af649f7c62de610bf43178425b75c
d4x337 <d4x337@protonmail.com>
2019-04-14 07:01:03 +0200

    Invoice model mass assigment vulnerability fix

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,12 +1,9 @@
 class Invoice < ActiveRecord::Base
  
    belongs_to :order
-   
    has_one :user
- 
    after_create :send_invoice
-
-
+   attr_accessible :user_id, :order_id, :invoice_date, :invoice_year, :email, :quantity, :subscription, :annual, :starts_on, :expires_on, :first_name, :last_name, :address, :zip, :town, :city, :country, :telephone, :total, :ip_address, :only_services, :payed, :delivered
 
    private
    def send_invitation
